cc400fb363952763675c172d391f205b3ba88295
NataliaBrandaodeSousa <al.natalia.sousa@impatech.edu.br>
2025-11-01 13:29:29 -0300

    adiciona metodo Simpson e alguns exemplos

--- integracao.py
+++ integracao.py
@@ -26,9 +26,10 @@ def integral(f, a, b, n, metodo = "trapezio"):
     F = [f(a)]
     X = [a]
 
-    def poligono(ponto, ponto2):
+    #polígonos de 4 lados
+    def poligono4(ponto, ponto2):
         x_vertices = [ponto[0],ponto2[0],ponto2[0],ponto[0]]
-        y_vertices = [ponto[1],ponto2[1],0,0]
+        y_vertices = [ponto[1],ponto2[1],0,0]                                                                                       
         plt.fill(x_vertices, y_vertices, color='skyblue', alpha=0.7)
 
     if metodo == 'trapezio':
@@ -41,7 +42,7 @@ def integral(f, a, b, n, metodo = "trapezio"):
             F.append(f_dir)
             X.append(x_dir)
             soma += f_xm * delta_x
-            poligono((x_esq,f_esq),(x_dir,f_dir))
+            poligono4((x_esq,f_esq),(x_dir,f_dir))
     elif metodo == 'ponto_medio':
         for i in range(n):
             x_esq = a + i * delta_x
@@ -50,8 +51,22 @@ def integral(f, a, b, n, metodo = "trapezio"):
             f_xm = f(x_med)
             F.append(f_xm)
             X.append(x_med)
-            soma += f_xm * delta_x
-            poligono((x_esq,f_xm),(x_dir,f_xm))
+            soma += ( f(x_esq) + f_xm*4 + f(x_dir))/6 * delta_x
+            poligono4((x_esq,f_xm),(x_dir,f_xm))
+
+    elif metodo == 'simpson':
+        for i in range(n):
+            x_esq = a + i * delta_x
+            x_dir = x_esq + delta_x
+            x_med = (x_esq + x_dir) / 2.0
+
+            f_esq = f(x_esq)
+            f_dir = f(x_dir)
+            f_xm = f(x_med)
+
+            soma += (f_esq + 4.0*f_xm + f_dir) * (delta_x / 6.0)
+         
+            
     
     F.append(f(b))
     X.append(b)
@@ -70,11 +85,16 @@ if __name__ == "__main__":
     g = lambda x: math.sin(x)
   
     area1 = integral(f, 0, 1, 4)
-    print(area1)  
+    print("função f com metodo do trapezio", area1)  
     area2 = integral(g, 0, math.pi, 100)
-    print(area2)   
+    print("função g com metodo do trapezio", area2)  
 
     area3 = integral(f, 0, 1, 4, 'ponto_medio')
-    print(area3)  
+    print("função f com metodo do ponto medio", area3)  
     area4 = integral(g, 0, math.pi, 100, 'ponto_medio')
-    print(area4)   
+    print("função g com metodo do ponto medio", area4)    
+
+    area5 = integral(f, 0, 1, 4, 'simpson')
+    print("função f com metodo do simpson", area5)  
+    area6 = integral(g, 0, math.pi, 100, 'simpson')
+    print("função g com metodo do simpson", area6)    
